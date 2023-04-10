4d58c3757c5521a61d2d6c2ce01421a26c072e46
Isaac Thill <ithill22@turing.edu>
2023-04-09 20:27:03 -0600

    complete user story 2

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,4 +2,13 @@ class Employee < ApplicationRecord
   belongs_to :department
   has_many :employee_tickets
   has_many :tickets, through: :employee_tickets
+
+  def tickets_by_age
+    tickets.order(age: :desc)
+  end
+
+  def oldest_ticket
+    ticket = tickets_by_age.first
+    ticket.subject
+  end
 end
--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,7 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
+  before :each do
+    test_data
+  end
+
   describe 'relationships' do
     it { should belong_to :department }
+    it { should have_many :employee_tickets }
+    it { should have_many(:tickets).through(:employee_tickets) }
+  end
+
+  describe 'instance methods' do
+    describe '#tickets_by_age' do
+      it 'returns tickets by age' do
+        expect(@employee_1.tickets_by_age).to eq([@ticket_3, @ticket_1])
+        expect(@employee_2.tickets_by_age).to eq([@ticket_2, @ticket_4])
+      end
+    end
+
+    describe '#oldest_ticket' do
+      it 'returns oldest ticket' do
+        expect(@employee_1.oldest_ticket).to eq(@ticket_3.subject)
+        expect(@employee_2.oldest_ticket).to eq(@ticket_2.subject)
+      end
+    end
   end
 end
